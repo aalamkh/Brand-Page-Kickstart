a7c6d6621860909b9492572a59e1b5927e0d1aae
Aalam <user@Aalam.home>
2023-12-29 16:46:16 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Hero from "./Components/Hero";
+import Navigate from "./Components/Navigate";
+function App() {
+  return (
+    <>
+      <Navigate/>
+      <Hero/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Navigate.jsx
@@ -0,0 +1,20 @@
+const Navigate = () => {
+    return (
+        <>
+          <nav className="container">
+            <div className="logo">
+             <img src="/public/images/brand_logo.png" alt="logo" />
+            </div>
+            <ul>
+              <li href="#">MENU</li>
+              <li href="#">LOCATION</li>
+              <li href="#">ABOUT</li>
+              <li href="#">CONTACT</li>
+            </ul>
+            <button>Login</button>
+          </nav>
+        </>
+      )
+    }
+
+export default Navigate;
